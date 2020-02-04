7b58fd97232d0ba489c756c6eef0e8b7e1afc2bc
Sam <sdressler318@gmail.com>
2020-02-04 17:18:30 -0600

    added progress on user interface and client side application

---
+++ UX/Login.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoneyHub
+{
+    public partial class Login : Form
+    {
+        #region Login
+
+        #region initialize variables
+        //Variables "192.168.51.209"
+        //byte[] ip = { 192, 168, 51, 209};
+        //Sam's pc ip
+        String ip = "192.168.51.209";
+        int port = 4444;
+        string message = "Test2";
+        ServerConnector client = new ServerConnector();
+        
+        //User currentUser = null;
+        User testUser = new User("test", "test");
+        Home application = new Home();
+        String userText = string.Empty;
+        String pwdText = string.Empty;
+        #endregion
+        #region Startup
+        public Login()
+        {
+            //this.client.ExecuteClient(ip, port, message);
+            InitializeComponent();
+        }
+        #endregion
+        #region User Attempts to Login 
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            userText = this.usernameText.Text;
+            pwdText = this.passwordText.Text;
+            if (!userText.Equals(string.Empty) && !pwdText.Equals(string.Empty))
+            {
+                this.credentialsInvalidText.Visible = false;
+                //TODO: Check the userText against the database to see if there is an 
+                //      account with that user name and then check the password if there is.
+                //for now the code will check to see if the user is test and password is test.
+                if (userText.Equals("test"))
+                {
+                    if (pwdText.Equals("test"))
+                    {
+                        Console.WriteLine(DateTime.Now + ": Login Succeeded, User: " + testUser.getUsername());
+                        application.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        Console.WriteLine(DateTime.Now + ": Login Credentials Invalid:");
+                        this.credentialsInvalidText.Text = "Password is incorrect";
+                        this.credentialsInvalidText.Visible = true;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(DateTime.Now + ": Username Invalid:");
+                    this.credentialsInvalidText.Text = "Username not recognized";
+                    this.credentialsInvalidText.Visible = true;
+                }
+            }
+            else
+            {
+                Console.WriteLine(DateTime.Now + ": Login Credentials Invalid:");
+                this.credentialsInvalidText.Visible = true;
+            }
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ UX/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoneyHub
+{
+    static class Program
+    {
+        //create a new login
+        public static Login login;
+        //Windows requires this
+        [STAThread]
+        //Enable console debug information to be visible
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        //variables
+        static extern bool AttachConsole(int dwProcessId);
+        private const int ATTACH_PARENT_PROCESS = -1;
+
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            AttachConsole(ATTACH_PARENT_PROCESS);
+            login = new Login();
+            Application.Run(login);
+        }
+    }
+}
